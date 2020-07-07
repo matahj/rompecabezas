e5ba560d59410b6901d830d9e952a9303956bbd4
matahj <matahj@gmail.com>
2020-07-06 23:10:15 -0500

    Funcona para 10 elementos, aveces se sueltan ¿porqué?

---
+++ js/main.js
@@ -0,0 +1,95 @@
+/*Programacion de JavaScript*/
+
+var piezas = document.getElementsByClassName('movil');
+
+var tamWidh = [44,44,44,44,44,44,44,44,44,44];
+var tamHeight = [49,49,49,49,49,49,49,49,49,49];
+
+for(var i=0;i<piezas.length;i++){
+	piezas[i].setAttribute("width", tamWidh[i]);
+	piezas[i].setAttribute("height",tamHeight[i]);
+	piezas[i].setAttribute("x", Math.floor((Math.random() * 760) + 50));
+	piezas[i].setAttribute("y", Math.floor((Math.random() * 100) + 521));
+	piezas[i].setAttribute("onmousedown","seleccionarElemento(evt)");
+}
+
+var elementSelect = 0;  
+var currentX = 0;
+var currentY = 0;
+var currentPosX = 0;
+var currentPosY = 0;
+
+function seleccionarElemento(evt) {
+	elementSelect = reordenar(evt);
+	currentX = evt.clientX;        
+	currentY = evt.clientY;
+	currentPosx = parseFloat(elementSelect.getAttribute("x"));     
+	currentPosy = parseFloat(elementSelect.getAttribute("y"));
+	elementSelect.setAttribute("onmousemove","moverElemento(evt)");
+}
+
+function moverElemento(evt){
+	var dx = evt.clientX - currentX;
+	var dy = evt.clientY - currentY;
+	currentPosx = currentPosx + dx;
+	currentPosy = currentPosy + dy;
+	elementSelect.setAttribute("x",currentPosx);
+	elementSelect.setAttribute("y",currentPosy);
+	currentX = evt.clientX;        
+	currentY = evt.clientY;
+	elementSelect.setAttribute("onmouseout","deseleccionarElemento(evt)");
+	elementSelect.setAttribute("onmouseup","deseleccionarElemento(evt)");
+	iman();
+}
+
+function deseleccionarElemento(evt){
+	testing();
+	if(elementSelect != 0){			
+		elementSelect.removeAttribute("onmousemove");
+		elementSelect.removeAttribute("onmouseout");
+		elementSelect.removeAttribute("onmouseup");
+		elementSelect = 0;
+	}
+}
+
+var entorno = document.getElementById('entorno');
+
+function reordenar(evt){
+	var padre = evt.target.parentNode;
+	var clone = padre.cloneNode(true);
+	var id = padre.getAttribute("id");
+	entorno.removeChild(document.getElementById(id));
+	entorno.appendChild(clone);
+	return entorno.lastChild.firstChild;
+}
+
+var origX = [51,817,51,96,591,637,682,727,772,817];   
+var origY = [51,54,101,101,103,103,103,103,103,103];
+
+function iman(){
+	for(var i=0;i<piezas.length;i++){
+		if (Math.abs(currentPosx-origX[i])<10 && Math.abs(currentPosy-origY[i])<10) {
+			elementSelect.setAttribute("x",origX[i]);
+			elementSelect.setAttribute("y",origY[i]);
+		}
+	}
+}
+			
+var win = document.getElementById("win");
+
+function testing() {
+	var bien_ubicada = 0;
+	var padres = document.getElementsByClassName('padre');
+	for(var i=0;i<piezas.length;i++){
+		var posx = parseFloat(padres[i].firstChild.getAttribute("x"));    
+		var posy = parseFloat(padres[i].firstChild.getAttribute("y"));
+		ide = padres[i].getAttribute("id");
+		if(origX[ide] == posx && origY[ide] == posy){
+			bien_ubicada = bien_ubicada + 1;
+		}
+	}
+	if(bien_ubicada == 10){
+		win.play();
+	}
+}
+
